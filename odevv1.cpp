b03bf887e744979b1fdf96e7f3c5b4c5823704c5
Şuayip <suayip_048@msn.com>
2020-04-01 18:40:28 +0300

    cift tek sayılar toplamı
    alınan sayıya kadarki tek veya cift sayıları toplama.

--- odevv1.cpp
+++ odevv1.cpp
@@ -3,30 +3,59 @@ using namespace std; //komut satýrýndan sayý al bu sayýya kadar olan tamsayýlarý
 int main(){
 
 setlocale(LC_ALL,"Turkish"); //türkçe karakter
-
+int toplam;
 int i;
 int a;
 cout <<"bir sayý giriniz: " << endl;
 cin >> a;
 
-cout << a << " e kadar olan tamsayýlar: " << endl;
 if(a >= 0){
-
-for( i=0; i<=a; i++)
+if(a%2 == 0){
+cout << a << " e kadar olan cift tamsayýlarýn toplamý: " << endl;
+for( i=0; i<=a; i+=2)
 {
-	cout << i << endl;
 	
-	 
+	toplam +=i;
+	cout << toplam << endl;
+}
+}
+else if (a%2 != 0){
+	cout << a << " e kadar olan tek tamsayýlarýn toplamý: " << endl;
+	for(i=1; i<=a; i+=2)
+	{
+	toplam +=i;
+	cout << toplam << endl;
+	}
+	
 }
 }
 else if(a < 0){
-	for(i=0; i>=a; i--)
+	if(a%2 == 0){
+		cout << a << " e kadar olan cift tamsayýlarýn toplamý: " << endl;
+		for( i=0; i >= a; i-=2)
 	{
-		cout << i << endl;
+		toplam +=i;
+		cout << toplam << endl;
 	}
 }
 
+else if(a%2 != 0){
+	cout << a << " e kadar olan tek tamsayýlarýn toplamý: " << endl;
+	for(i=-1; i >= a; i-=2)
+	{
+		toplam +=i;
+		cout << toplam << endl;
+	}
+}
+}
 
+
+
+
+
+
+
+return 0;
 }
 	
 
