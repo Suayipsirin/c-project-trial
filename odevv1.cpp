57ce651783c5fec9ef77d27c561143eb32e77f49
Şuayip <suayip_048@msn.com>
2020-04-05 20:44:52 +0300

    Update odevv1.cpp

--- odevv1.cpp
+++ odevv1.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 #include <math.h>
-using namespace std; //komut satirindan sayi al bu sayiiya kadar olan tamsayilari bastir.
+using namespace std; //komut satirindan sayi al bu sayiya kadar olan tamsayilari bastir.
 
+int  mutlak(int sayi) {
+
+	if(sayi<0) {
+		sayi=sayi*-1;
+       
+}
+      return sayi;
+}
 void ciftler(int target) {
   int toplam = 0;
-  for(int i=0;i<=abs(target);i++) {
+  for(int i=0;i<= mutlak(target);i++) {
     if (i % 2 == 0) {
       toplam += i;
     }
   }
   
-  int sonuc = toplam*(target/abs(target));
+  int sonuc = toplam*(target/mutlak(target));
   cout << target << " e kadar olan cift tamsayilarin toplami: " << sonuc << endl;
 }
 
 void tekler(int target) {
   int toplam = 0;
-  for(int i=0;i<=abs(target);i++) {
+  for(int i=0;i<= mutlak(target);i++) {
     if (i % 2 != 0) {
       toplam += i;
     }
   }
-
-
-  
-   int sonuc = toplam*(target/abs(target));
+   int sonuc = toplam*(target/ mutlak(target));
   cout << target << " e kadar olan tek tamsayilarin toplami: " << sonuc << endl;
 }
 
